ce9d1410f677c51ef370e26e500ff68933728eaa
austenc <austen.cameron@gmail.com>
2018-03-27 12:49:19 -0600

    simple first version with double mustache tag

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,99 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import { window, commands, Disposable, ExtensionContext } from 'vscode';
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "laravel-blade-spacer" is now active!');
+const undoOptions = { undoStopBefore: false, undoStopAfter: true };
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+export function activate(context: ExtensionContext) {
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    console.log('The "Laravel Blade Spacer" extension is now active!');
 
-    context.subscriptions.push(disposable);
+    let spacer = new Spacer();
+    let controller = new SpacerController(spacer);
+
+    context.subscriptions.push(spacer);
+    context.subscriptions.push(controller);
 }
+
+class CursorMoveCommand {
+
+    constructor(private to: string, private by?: string) {
+    }
+
+    public createCommand(args?: any) {
+        let cursorMoveArgs: any = {
+            to: this.to,
+            by: this.by,
+            value: args.repeat || 1,
+            select: !!args.isVisual
+        }
+        return {
+            commandId: 'cursorMove',
+            args: cursorMoveArgs
+        };
+    }
+}
+
+class Spacer {
+    public space() {
+        const editor = window.activeTextEditor;
+        let moveBy = false;
+
+        if (! editor) {
+            return;
+        }
+
+        editor.selections.map(selection => {
+            let pos = selection.active;
+            let line = editor.document.lineAt(pos.line).text;
+            let before = line.slice(Math.max(pos.character - 2, 0), pos.character);
+            let after = line.slice(pos.character, pos.character + 2);
+
+            if (before === '{{' && after === '}}') {
+                editor.edit(edit => {
+                    edit.insert(pos, '  ');
+                }, undoOptions);                
+
+                moveBy = { to: 'left', by: 'character' }
+            }
+            
+            return selection;
+        });
+        
+        if (moveBy) {
+            commands.executeCommand('cursorMove', moveBy)
+
+        }
+    }
+
+    dispose() {
+    
+    }
+}
+
+class SpacerController {
+    private spacer: Spacer;
+    private disposable: Disposable;
+
+    constructor(spacer: Spacer) {
+        this.spacer = spacer;
+
+        let subscriptions: Disposable[] = [];
+        window.onDidChangeTextEditorSelection(this.onEvent, this, subscriptions);
+        window.onDidChangeActiveTextEditor(this.onEvent, this, subscriptions);
+
+        this.disposable = Disposable.from(...subscriptions);
+    }
+
+    private onEvent() {
+        this.spacer.space();
+    }
+
+    public dispose() {
+        this.disposable.dispose();
+    }
+}
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {
